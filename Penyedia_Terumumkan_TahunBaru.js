0f630f3e8c674c653bddb076f5b26bcb9e257f73
unknown <denigandawijayad@yahoo.com>
2024-11-11 10:00:49 +0700

    update data for 2025

--- Penyedia_Terumumkan_TahunBaru.js
+++ Penyedia_Terumumkan_TahunBaru.js
@@ -16,8 +16,8 @@ const url = process.env.API_URL_PEN; // Use environment variable for URL
 
 // Define the spreadsheet ID and range
 const spreadsheetId = process.env.SPREADSHEET_ID; // Use environment variable for Spreadsheet ID
-const clearRange = 'Penyedia!A:ZZ'; // Range to clear
-const updateRange = 'Penyedia!A1'; // Range to update
+const clearRange = 'PenyediaBR!A:ZZ'; // Range to clear
+const updateRange = 'PenyediaBR!A1'; // Range to update
 
 async function fetchData() {
     try {
